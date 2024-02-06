73fad9d94f20f77781420f83026e0457af1a89db
Andrew Miranda <tiburonandrew@gmail.com>
2024-02-05 21:27:10 -0500

    Change Port

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def index():
     return render_template('index.html', libros=libros_ordenados, filtro_autor=filtro_autor)
 
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8080)
